4fd488ad8121952ff588e74b48910a7d99bbbdec
S.M ANZUMUL JUBAYER <anzumuljubayer222@gmail.com>
2025-11-09 10:42:00 +0600

    client-setup

---
+++ src/Router/router.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router';
+import App from '../App';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<App></App>,
+  },
+]);
+
+
+export default router;
